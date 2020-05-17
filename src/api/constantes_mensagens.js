5e76b498698b309324de4eccf27fd57b53f2a84d
Renato Mateus <renatomateusx@gmail.com>
2020-05-17 13:57:39 -0300

    commit

---
+++ src/api/constantes_mensagens.js
@@ -0,0 +1,3 @@
+export default Object.freeze({
+  MENSAGEM_BOLETO: 'Olá, {first_name},%0a %0a %0aInformamos que recebemos a ordem do seu pedido para o produto *{produto_name}* e ele já está reservado!   %0a Para trazer mais facilidade para você, segue código de barras do boleto gerado no ato da compra: *{boleto_bar_code}* e, também, o link para acessá-lo de qualquer lugar. %0a Qualquer dúvida pode nos contactar através do nosso e-mail. Parabéns pela compra. Link para acessar o seu boleto: {link_boleto}', 
+});
--- src/views/Pages/Pedidos.vue
+++ src/views/Pages/Pedidos.vue
@@ -262,6 +262,10 @@ option {
                     >
                       <option value="-1">&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspEnvie uma Mensagem</option>
                       <option
+                        value="0"
+                        v-show="metodo == 'bolbradesco' || metodo == 'BOLETO'"
+                      >&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspMensagem de Boleto</option>
+                      <option
                         v-for="{id_mensagem, nome_mensagem} in arrayWhatsAppMessage"
                         :key="id_mensagem"
                         :value="id_mensagem"
@@ -314,6 +318,7 @@ import API_MENSAGERIA from "../../api/mensageriaAPI";
 
 import vSelect from "vue-select";
 import "vue-select/dist/vue-select.css";
+import constantes_mensagens from "../../api/constantes_mensagens";
 Vue.component("v-select", vSelect);
 
 TimeAgo.addLocale(pt);
@@ -432,7 +437,7 @@ export default {
               sessionStorage.setItem("DadosLoja", JSON.stringify(resLoja.data));
               API_MENSAGERIA.GetMensagensWhatsApp()
                 .then(resMensagensWhats => {
-                  console.log(resMensagensWhats.data);
+                  //console.log(resMensagensWhats.data);
                   this.arrayWhatsAppMessageOriginal = resMensagensWhats.data;
                   resMensagensWhats.data.forEach((obj, i) => {
                     this.arrayWhatsAppMessage.push({
@@ -490,7 +495,8 @@ export default {
                         Date.now(),
                         "time"
                       ),
-                      json_front_end_user_data: obj.json_front_end_user_data
+                      json_front_end_user_data: obj.json_front_end_user_data,
+                      json_gw_response: obj.json_gw_response
                     });
 
                     //console.log(Date.now(), Date.parse(LData));
@@ -613,27 +619,91 @@ export default {
     selecionaMensagemEnviar(event) {
       this.mensagemEnviarSelecionada = event.target.value;
     },
-    enviarMensagemAcao(id, id_mensagem) {
-      const LURL =
-        "https://api.whatsapp.com/send?phone={@PHONE}&text={@MENSAGEM}";
-      var Mensagem = this.arrayWhatsAppMessageOriginal.find(
-        x => x.id == id_mensagem
-      ).mensagem;
-      const Telefone =
-        "55" +
-        JSON.parse(
+    async enviarMensagemAcao(id, id_mensagem) {
+      if (id_mensagem > 0) {
+        const LURL =
+          "https://api.whatsapp.com/send?phone={@PHONE}&text={@MENSAGEM}";
+        var Mensagem = this.arrayWhatsAppMessageOriginal.find(
+          x => x.id == id_mensagem
+        ).mensagem;
+        const Telefone =
+          "55" +
+          JSON.parse(
+            this.gridData.find(x => x.id == id).json_front_end_user_data
+          ).dadosComprador.telefone.replace(/[^0-9\.]+/g, "");
+        const Nome = JSON.parse(
           this.gridData.find(x => x.id == id).json_front_end_user_data
-        ).dadosComprador.telefone.replace(/[^0-9\.]+/g, "");
-      const Nome = JSON.parse(
-        this.gridData.find(x => x.id == id).json_front_end_user_data
-      ).dadosComprador.nome_completo.split(" ")[0];
-      Mensagem = Mensagem.replace("{first_name}", Nome);
-      const LURLFinal = LURL.replace("{@PHONE}", Telefone).replace(
-        "{@MENSAGEM}",
-        Mensagem
-      );
-      var win = window.open(LURLFinal, "_blank");
-      win.focus();
+        ).dadosComprador.nome_completo.split(" ")[0];
+        Mensagem = Mensagem.replace("{first_name}", "%0a" + Nome + "%0a");
+        const LURLFinal = LURL.replace("{@PHONE}", Telefone).replace(
+          "{@MENSAGEM}",
+          Mensagem
+        );
+        var win = window.open(LURLFinal, "_blank");
+        win.focus();
+      } else if (id_mensagem == 0) {
+        const LURL =
+          "https://api.whatsapp.com/send?phone={@PHONE}&text={@MENSAGEM}";
+        var Mensagem = constantes_mensagens.MENSAGEM_BOLETO;
+        const Telefone =
+          "55" +
+          JSON.parse(
+            this.gridData.find(x => x.id == id).json_front_end_user_data
+          ).dadosComprador.telefone.replace(/[^0-9\.]+/g, "");
+        const DadosBoleto = JSON.parse(
+          this.gridData.find(x => x.id == id).json_gw_response
+        );
+        const LBarCode = this.getBarCodeBoleto(DadosBoleto);
+        const LProdutos = JSON.parse(
+          this.gridData.find(x => x.id == id).json_front_end_user_data
+        ).produtos;
+        const Link = JSON.parse(this.gridData.find(x => x.id == id).json_gw_response);
+        const LlinkBoleto = await this.getLinkBoleto(Link);
+        var LprodutoTitle = await this.getProdutoName(LProdutos);
+        const Nome = JSON.parse(this.gridData.find(x => x.id == id).json_front_end_user_data).dadosComprador.nome_completo.split(" ")[0];
+        Mensagem = Mensagem.replace("{first_name}", "%0a" + Nome + "%0a");
+        Mensagem = Mensagem.replace("{produto_name}", LprodutoTitle);
+        Mensagem = Mensagem.replace("{boleto_bar_code}", LBarCode);
+        Mensagem = Mensagem.replace("{link_boleto}.", LlinkBoleto);
+        const LURLFinal = LURL.replace("{@PHONE}", Telefone).replace("{@MENSAGEM}", Mensagem);
+        var win = window.open(LURLFinal, "_blank");
+        win.focus();
+      }
+    },
+    getBarCodeBoleto(obj) {
+      const LJSON = obj;
+      if (LJSON.hasOwnProperty("payment_method_id"))
+        return LJSON.barcode.content;
+      if (
+        LJSON.hasOwnProperty("payment_method") &&
+        LJSON.payment_method.hasOwnProperty("boleto")
+      )
+        return LJSON.payment_method.boleto.formatted_barcode;
+
+      return "";
+    },
+    getProdutoName(arrayProdutos) {
+      return new Promise((resolve, reject) => {
+        var LprodutoTitle = "";
+        arrayProdutos.forEach((obj, i) => {
+          LprodutoTitle = LprodutoTitle + obj.title + ", ";
+        });
+        LprodutoTitle = LprodutoTitle.substring(0, LprodutoTitle.length - 1);
+        LprodutoTitle = LprodutoTitle.replace(/.$/,".", " e ");
+        resolve(LprodutoTitle);
+      });
+    },
+    getLinkBoleto(obj){
+      const LJSON = obj;
+        if (LJSON.hasOwnProperty("payment_method_id"))
+          return LJSON.transaction_details.external_resource_url;
+        if (
+          LJSON.hasOwnProperty("payment_method") &&
+          LJSON.payment_method.hasOwnProperty("boleto")
+        )
+          return LJSON.payment_method.links.find(
+            x => x.media == "application/pdf"
+          ).href;
     }
   }
 };
